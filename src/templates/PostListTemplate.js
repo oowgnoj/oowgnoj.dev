7cb302107f2a8c20f8e7f6af3a4f49a4751f235a
oowgnoj <mangtaob@gmail.com>
2020-05-10 22:05:19 +0900

    delte console

--- src/templates/PostListTemplate.js
+++ src/templates/PostListTemplate.js
@@ -1,85 +1,78 @@
-  
 import React from 'react';
 import PostCategory from '../components/categories';
 import SEO from '../components/seo';
-import { useStaticQuery, Link } from "gatsby"
-import Layout from "../components/layout"
-import Categories from "../components/categories"
-import styled from "styled-components"
+import { useStaticQuery, Link } from 'gatsby';
+import Layout from '../components/layout';
+import Categories from '../components/categories';
+import styled from 'styled-components';
 
+const PostListTemplate = ({ location, pageContext }) => {
+    const { cagtegory, pagePath, nodes } = pageContext;
+    const current = location.state ? location.state.current : undefined;
 
-const PostListTemplate = ({location, pageContext}) => {
-  const { cagtegory, pagePath, nodes} = pageContext;
-  const current = location.state ? location.state.current : undefined;
-
-  return (
-    <Layout isMain={true}>
-    <SEO title="Home" />
-    <Categories current={current}></Categories>
-        {nodes.map( node => (
-          <PostItem key={node.id}>
-            <PostItemWrapper>
-            {console.log(node.frontmatter)}
-              <Link to={node.frontmatter.title}  style={{textDecoration: `none`}}>
-                <PostTitle> {node.frontmatter.title} </PostTitle>
-                <PostSubtitle>{node.frontmatter.subtitle}</PostSubtitle>
-                <PostDate>{node.frontmatter.date? node.frontmatter.date.slice(0,10):null}</PostDate>
-              </Link>
-            </PostItemWrapper>
-          </PostItem>
-        ))}
-  </Layout>
-  );
+    return (
+        <Layout isMain={true}>
+            <SEO title="Home" />
+            <Categories current={current}></Categories>
+            {nodes.map(node => (
+                <PostItem key={node.id}>
+                    <PostItemWrapper>
+                        <Link to={node.frontmatter.title} style={{ textDecoration: `none` }}>
+                            <PostTitle> {node.frontmatter.title} </PostTitle>
+                            <PostSubtitle>{node.frontmatter.subtitle}</PostSubtitle>
+                            <PostDate>{node.frontmatter.date ? node.frontmatter.date.slice(0, 10) : null}</PostDate>
+                        </Link>
+                    </PostItemWrapper>
+                </PostItem>
+            ))}
+        </Layout>
+    );
 };
-
 
 export default PostListTemplate;
 
-
 const PostItem = styled.li`
-  list-style: none;
-`
+    list-style: none;
+`;
 
 const PostItemWrapper = styled.div`
-  padding-top: 20px;
-  &:hover{
-    box-shadow:inset 0 -3px 0 #90AFC5;
-  }
-`
+    padding-top: 20px;
+    &:hover {
+        box-shadow: inset 0 -3px 0 #90afc5;
+    }
+`;
 const PostTitle = styled.div`
-  font-size: 26px;
-  color: rgb(72,72,72);
-  font-weight: bold;
-  padding-bottom: 5px;
-  @media(max-width:767px){
-    font-size: 21px;  
-  }
-`
+    font-size: 26px;
+    color: rgb(72, 72, 72);
+    font-weight: bold;
+    padding-bottom: 5px;
+    @media (max-width: 767px) {
+        font-size: 21px;
+    }
+`;
 
 const PostSubtitle = styled.div`
-  font-size: 16px;
-  color: #484848;
-  padding-bottom: 5px;
-  @media(max-width:767px){
-    font-size: 15px;  
-  }
-  
-`
+    font-size: 16px;
+    color: #484848;
+    padding-bottom: 5px;
+    @media (max-width: 767px) {
+        font-size: 15px;
+    }
+`;
 
 const PostPreview = styled.div`
-  font-size: 14px;
-  color: #484848;
-  padding-bottom: 5px;
-  @media(max-width:767px){
-    font-size: 12px;  
-  }
-
-`
+    font-size: 14px;
+    color: #484848;
+    padding-bottom: 5px;
+    @media (max-width: 767px) {
+        font-size: 12px;
+    }
+`;
 const PostDate = styled.div`
-  font-size: 13px;
-  color: #484848;
-  padding-bottom: 5px;
-  @media(max-width:767px){
-    font-size: 12px;  
-  }
-`
+    font-size: 13px;
+    color: #484848;
+    padding-bottom: 5px;
+    @media (max-width: 767px) {
+        font-size: 12px;
+    }
+`;
