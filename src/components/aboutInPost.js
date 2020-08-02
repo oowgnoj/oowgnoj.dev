3f9f5269b31be9334192cd3d53fdc3ad3f2714d9
jongwoo <jongwoo@atommerce.com>
2020-08-02 12:21:24 +0900

    july review draft

--- src/components/aboutInPost.js
+++ src/components/aboutInPost.js
@@ -1,55 +1,67 @@
-import React, {Fragment} from "react"
-import { useStaticQuery, Link } from "gatsby"
+import React, { Fragment } from 'react';
+import { useStaticQuery, Link } from 'gatsby';
 import { Query } from '../../graphql-types';
-import styled from "styled-components"
-import Me from "./../images/me.png"
+import styled from 'styled-components';
+import Me from './../images/me.png';
 
-
-const AboutInpost = ({item}) => {
-  const linkto = 'https://github.com/oowgnoj';
-  return(
-    <Warpper>
-        <hr />
-        <img src={Me} style={{ borderRadius:'50%', width:'80px', height: '80px', display:'flex', margin: '0px'}}/>
-        <About> 반복의 느린 變化 
-        <LineBreak/>
-          <Nickname> oowgnoj <a style={{textDecoration: `none`}} href={linkto}> <LinkItem> github </LinkItem> </a></Nickname>
-        </About>
-
-    </Warpper>
+const AboutInpost = ({ item }) => {
+    const linkto = 'https://github.com/oowgnoj';
+    return (
+        <Warpper>
+            <Img src={Me} />
+            <About>
+                {' '}
+                반복의 느린 變化
+                <LineBreak />
+                <Nickname>
+                    {' '}
+                    oowgnoj{' '}
+                    <a style={{ textDecoration: `none` }} href={linkto}>
+                        {' '}
+                        <LinkItem> github </LinkItem>{' '}
+                    </a>
+                </Nickname>
+            </About>
+        </Warpper>
     );
-}
+};
 
-export default AboutInpost
+export default AboutInpost;
 // 반복의 느린 變化
 
 const Warpper = styled.div`
-  display: flex;
-  margin-top: 100px;
-  justify-content: flex-start;
-  flex-wrap: wrap;
-  margin-bottom: 100px;
-`
+    display: flex;
+    margin-top: 100px;
+    justify-contents: flex-start;
+    flex-wrap: wrap;
+    margin-bottom: 100px;
+`;
+const Img = styled.img`
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+    display: flex;
+    margin: 0;
+`;
 
 const About = styled.div`
-  padding-left : 20px;
-  padding-top: 6px;
-  width: 70%;
-`
+    padding-left: 20px;
+    padding-top: 6px;
+    width: 70%;
+`;
 
 const Nickname = styled.div`
-  margin-top: 6px;
-`
+    margin-top: 6px;
+`;
 const LineBreak = styled.div`
-  width: 100%;
-`
-
+    width: 100%;
+`;
 
 const LinkItem = styled.span`
-  color: grey;
-  cursor: pointer;
-  display: flex;
-  width: auto;
-  margin-top: 7px;
-  display: inline;
-`
+    color: grey;
+    cursor: pointer;
+    display: flex;
+    width: auto;
+    margin-top: 7px;
+    display: inline;
+`;
