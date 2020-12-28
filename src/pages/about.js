f679c8e02c885a5155cc5e15e0514c067e5c143e
oowgnoj <mangtaob@gmail.com>
2020-12-28 21:11:06 +0900

    CHANGE intro

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,10 +12,9 @@ export default () => {
             <Contents>
                 안녕하세요. 박종우 입니다.
                 <p>
-                    학부에서 데이터 분석으로 처음 프로그래밍을 시작했습니다. 2020년에는{' '}
-                    <a href="http://www.atommerce.com/">아토머스</a>에서 웹 프론트를 개발하며 개발자로 지내고 있습니다.
-                    프로그래밍의 매력은 원하는 것을 만들수 있는 점이라고 생각합니다. 웹을 처음 배우며 주머니 사정에
-                    맞추어 여행지를 알아보고자{' '}
+                    데이터 분석으로 처음 프로그래밍을 시작했습니다. 2020 년에는{' '}
+                    <a href="http://www.atommerce.com/">아토머스</a>에서 프론트엔드를 개발했습니다. 생각을 실체로 구현할
+                    수 있어 프로그래밍을 좋아합니다. 웹을 처음 배우며 주머니 사정에 맞추어 여행지를 알아보고자{' '}
                     <a
                         href="https://www.notion.so/oowgonj/Travel-Calculator-28ccc8f49ad241cda94894985d2a23cf"
                         target="_blank"
@@ -29,7 +28,7 @@ export default () => {
                     {', '}
                     <a href="https://www.notion.so/oowgonj/MYRO-b7fa1126cc3241fab699db93de66c9ae" target="_blank">
                         습관
-                    </a>
+                    </a>{' '}
                     등 관심있는 것을 재미있게 개발하고 있습니다.
                 </p>
                 <br />
